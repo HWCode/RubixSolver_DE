6763c876b67d3d9d65677174c7dd975f0a65d360
HWCode <eftodie@gmail.com>
2017-02-21 20:10:25 -0800

    Added NeuralNetwork stuff

---
+++ RubixSolver/Heuristic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubixSolver
+{
+    class Heuristic
+    {
+        public static int manhattan() {
+            int x_coord, y_coord, z_coord;
+
+
+            return 1;
+        }
+
+    }
+}
---
+++ RubixSolver/Neural Network/Formula.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubixSolver
+{
+    class Formula
+    {
+        public static int NeuralNetworkError() {
+            return 0;
+        }
+
+        public static int SigmaK()
+        {
+            return 0;
+        }
+
+        public static int DeltaKJ()
+        {
+            return 0;
+        }
+
+        public static int SigmaJ()
+        {
+            return 0;
+        }
+
+        public static int DeltaJI()
+        {
+            return 0;
+        }
+    }
+}
